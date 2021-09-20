042be91194da7930100a71c540e3fb822ba03cee
Valeria Hurava <ler4ik65@gmail.com>
2021-09-20 13:35:14 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ public class Main {
         BonusMilesService service = new BonusMilesService();
 
         int onlyMiles1 = service.calculate(20, 10_000);
-        System.out.println(onlyMiles1);
+        System.out.println("Цена билета: 10 000 рублей; " + "Начисленные мили за билет: " + onlyMiles1);
 
         int onlyMiles2 = service.calculate(20, 200);
-        System.out.println(onlyMiles2);
+        System.out.println("Цена билета: 200 рублей; " + "Начисленные мили за билет: " + onlyMiles2);
 
         int onlyMiles3 = service.calculate(20, 6_000);
-        System.out.println(onlyMiles3);
+        System.out.println("Цена билета: 6 000 рублей; " + "Начисленные мили за билет: " + onlyMiles3);
 
         int onlyMiles4 = service.calculate(20, 124_020);
-        System.out.println(onlyMiles4);
+        System.out.println("Цена билета: 124 020 рублей; " + "Начисленные мили за билет: " + onlyMiles4);
 
         int onlyMiles5 = service.calculate(20, 0);
-        System.out.println(onlyMiles5);
+        System.out.println("Цена билета: 0 рублей; " + "Начисленные мили за билет: " + onlyMiles5);
     }
 }
