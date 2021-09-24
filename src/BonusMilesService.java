e6b0178203a284b7f12b22dc1c8f5f00bdf88c5d
Valeria Hurava <ler4ik65@gmail.com>
2021-09-24 21:40:22 +0200

    modification commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
-    public int calculate(int priceOfOneBonusMile, int ticketPrice) {
-        int onlyMiles = ticketPrice / priceOfOneBonusMile;
+    public int calculate(int ticketPrice) {
+        int onlyMiles = ticketPrice / 20;
         return onlyMiles;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
 
-        int onlyMiles1 = service.calculate(20, 10_000);
+        int onlyMiles1 = service.calculate(10_000);
         System.out.println("Цена билета: 10 000 рублей; " + "Начисленные мили за билет: " + onlyMiles1);
 
-        int onlyMiles2 = service.calculate(20, 200);
+        int onlyMiles2 = service.calculate(200);
         System.out.println("Цена билета: 200 рублей; " + "Начисленные мили за билет: " + onlyMiles2);
 
-        int onlyMiles3 = service.calculate(20, 6_000);
+        int onlyMiles3 = service.calculate(6_000);
         System.out.println("Цена билета: 6 000 рублей; " + "Начисленные мили за билет: " + onlyMiles3);
 
-        int onlyMiles4 = service.calculate(20, 124_020);
+        int onlyMiles4 = service.calculate(124_020);
         System.out.println("Цена билета: 124 020 рублей; " + "Начисленные мили за билет: " + onlyMiles4);
 
-        int onlyMiles5 = service.calculate(20, 0);
+        int onlyMiles5 = service.calculate(0);
         System.out.println("Цена билета: 0 рублей; " + "Начисленные мили за билет: " + onlyMiles5);
     }
 }
